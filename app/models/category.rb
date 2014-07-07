60b672861a1b8cd863317ca235ae62af80b51ee2
TeamRory <rorycteam@gmail.com>
2014-07-07 16:37:11 -0400

    Added uniqness index for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :trivia_facts
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707182207) do
+ActiveRecord::Schema.define(version: 20140707203355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140707182207) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
 
   create_table "trivia_facts", force: true do |t|
     t.string   "question",    limit: 2000,                 null: false
