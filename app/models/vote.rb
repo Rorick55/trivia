ca4ca96f2dcc567eba8a9519631d2fc9a7786591
TeamRory <rorycteam@gmail.com>
2014-07-08 12:04:43 -0400

    vote model test passing

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :trivia_fact
 
-  validates :user
-  validates :trivia_fact
+  validates :user, presence: true
+  validates :trivia_fact, presence: true
+  validates :user_id, uniqueness: { scope: :trivia_fact_id }
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe Vote, type: :model do
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:trivia_fact) }
 
-  it { should validate_uniqueness_of(:user_id).scoped(:trivia_fact_id)}
+  it { should validate_uniqueness_of(:user_id).scoped_to(:trivia_fact_id) }
 
 end
