39648e6dec1fd252da1a4bcdab40c2e17d72feb3
TeamRory <rorycteam@gmail.com>
2014-07-09 16:15:18 -0400

    update method created

--- app/controllers/admin/trivia_facts_controller.rb
+++ app/controllers/admin/trivia_facts_controller.rb
@@ -9,15 +9,27 @@ class Admin::TriviaFactsController < ApplicationController
     @trivia_fact = TriviaFact.find(params[:id])
   end
 
+  def update
+    @trivia_fact = TriviaFact.find(params[:id])
+    if @trivia_fact.update(trivia_fact_params)
+      flash[:notice] = 'Successfully updated trivia fact!'
+      redirect_to admin_trivia_fact_path(@trivia_fact)
+    else
+      render :show
+    end
+  end
+
+
+
 
 
   private
 
   def trivia_fact_params
-    params.require(:trivia_fact).permit(:question, :answer, :category_id)
+    params.require(:trivia_fact).permit(:question, :answer, :category_id, :featured)
   end
 
-    def authorize_admin
+  def authorize_admin
     unless current_user.is_admin?
       raise ActionController::RoutingError.new('Not Found')
     end
