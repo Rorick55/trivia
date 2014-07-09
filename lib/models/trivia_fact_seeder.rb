1ca638b6929631e9d8664e9559ed112d43d10304
TeamRory <rorycteam@gmail.com>
2014-07-09 08:57:46 -0400

    Added class for trivia seeder

---
+++ lib/models/trivia_fact_seeder.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+
+class TriviaStack
+  def initialize(url)
+    @url = url
+    @doc = Nokogiri::HTML(open(@url))
+  end
+
+  def category
+    location = @doc.css('.location').text
+    category_with_num = location.split('>').pop
+    category = category_with_num.split(' ')
+    category.pop
+    if category.include?('TRIVIA') || category.include?('Trivia')
+      category.pop
+    end
+    category = category.join(' ')
+  end
+
+  def create_category
+    Category.find_or_create_by!(name: category)
+  end
+
+  def create_stack
+    @doc.css('p').each do |item|
+      if item.text.include?('A:')
+        ques_ans_array = item.text.split('A:')
+        question = ques_ans_array[0].split.join(' ')
+        answer = ques_ans_array[1].split.join(' ')
+        TrivaFact.find_or_create_by(question: question, answer: answer, category: create_category)
+      end
+    end
+  end
+end
