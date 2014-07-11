004cbc5a7abd9af087f39bbfd0b9ebf8685877d2
TeamRory <rorycteam@gmail.com>
2014-07-11 14:21:26 -0400

    changed categories in seeder

--- lib/models/trivia_fact_seeder.rb
+++ lib/models/trivia_fact_seeder.rb
@@ -15,6 +15,35 @@ class TriviaFactSeeder
       category.pop
     end
     category = category.join(' ')
+    if ['History', 'American History', 'World History'].include? category
+      return 'History'
+    elsif ['Animal', 'Nature'].include? category
+      return 'Animals and Nature'
+    elsif ['American', 'U.S.', 'Cities and States', 'Presidential'].include? category
+      return 'America'
+    elsif ['Cinema', 'TV', 'Cartoons', 'Movie', 'Cinema and TV'].include? category
+      return 'Television and Cinema'
+    elsif ['Arts', 'Dance', 'Literature', 'Music'].include? category
+      return 'Arts and Literature'
+    elsif ['People', 'Famous People'].include? category
+      return 'People'
+    elsif ['World', 'International'].include? category
+      return 'International'
+    elsif ['Food', 'Cocktails and Drinks'].include? category
+      return 'Food'
+    elsif ['Miscellaneous', 'Misc', 'Easy', 'Funny', 'General', 'General Knowledge', 'Harder', 'Kids', 'Business'].include? category
+      return 'Miscellaneous'
+    elsif ['Language', 'Word'].include? category
+      return 'Language'
+    elsif ['Stupid Laws', 'Legal'].include? category
+      return 'Legal'
+    elsif ['Sports', 'International Sports'].include? category
+      return 'Sports'
+    elsif ['Science', 'Space', 'Medical', 'Human Body'].include? category
+      return 'Science'
+    else
+      return category
+    end
   end
 
   def create_category
@@ -32,3 +61,108 @@ class TriviaFactSeeder
     end
   end
 end
+
+
+
+
+
+
+
+["Animal",
+  "History",
+ "American History",
+ "World History",
+ "American",
+ "Arts",
+ "Cinema",
+ "TV",
+ "Business",
+ "Cartoons",
+ "Dance",
+ "Easy",
+ "Food",
+ "Cocktails & Drinks",
+ "Funny",
+ "General",
+ "General Knowledge",
+ "Harder",
+ "International",
+ "Invention",
+ "Kids",
+ "Language",
+ "Stupid Laws",
+ "Legal",
+ "Literature",
+ "Medical",
+ "Human Body",
+ "Cinema and TV",
+ "Military",
+ "Misc",
+ "Miscellaneous",
+ "Movie",
+ "Music",
+ "Mythology",
+ "Nature",
+ "People",
+ "Famous People",
+ "Cities and States",
+ "Politics",
+ "Presidential",
+ "Science",
+ "Space",
+ "Sports",
+ "International Sports",
+ "U.S.",
+ "Word",
+ "World"]
+
+
+
+
+["Animal",
+  "History",
+ "American History",
+ "World History",
+ "American",
+ "Arts",
+ "Cinema",
+ "TV",
+ "Business",
+ "Cartoons",
+ "Dance",
+ "Easy",
+ "Food",
+ "Cocktails & Drinks",
+ "Funny",
+ "General",
+ "General Knowledge",
+ "Harder",
+ "International",
+ "Invention",
+ "Kids",
+ "Language",
+ "Stupid Laws",
+ "Legal",
+ "Literature",
+ "Medical",
+ "Human Body",
+ "Cinema and TV",
+ "Military",
+ "Misc",
+ "Miscellaneous",
+ "Movie",
+ "Music",
+ "Mythology",
+ "Nature",
+ "People",
+ "Famous People",
+ "Cities and States",
+ "Politics",
+ "Presidential",
+ "Science",
+ "Space",
+ "Sports",
+ "International Sports",
+ "U.S.",
+ "Word",
+ "World"]
