cc7e2409223c9fffc6fe333d1993facded9d0127
TeamRory <rorycteam@gmail.com>
2014-07-14 08:56:43 -0400

    Added new and create actions for quiz response

--- app/controllers/quiz_responses_controller.rb
+++ app/controllers/quiz_responses_controller.rb
@@ -1,8 +1,18 @@
 class QuizResponsesController < ApplicationController
   def new
+    @quiz_questions = Quiz.random_quiz
+    @quiz_response = QuizResponse.new
   end
 
   def create
+    @quiz_response = QuizResponse.new(quiz_response_params)
+    @quiz_response.user = current_user
+    if @quiz_response.save
+      flash[:notice] = @quiz_response.score_response
+      redirect_to quiz_path(@quiz_response.quiz)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -11,4 +21,9 @@ class QuizResponsesController < ApplicationController
   def update
   end
 
+  private
+
+  def quiz_response_params
+    params.require(:quiz_response).permit(:answer1, :answer2, :answer3, :answer4, :answer5, :answer6, :answer7, :answer8, :answer9, :answer10, :quiz_id)
+  end
 end
