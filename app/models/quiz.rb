a3fb63df38d699d562040b663d2bdf6ecb1b5dd0
TeamRory <rorycteam@gmail.com>
2014-07-14 08:40:59 -0400

    set up quiz models

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,11 @@
 class Quiz < ActiveRecord::Base
+  has_many :quiz_responses
+  has_many :users, though: :quiz_responses
+  validates :trivia_questions, presence: true
+
+  def self.random_quiz
+    quiz = []
+    10.times { quiz << TriviaFact.order('RANDOM()').first }
+    quiz
+  end
 end
--- app/models/quiz_response.rb
+++ app/models/quiz_response.rb
@@ -1,2 +1,17 @@
 class QuizResponse < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :quiz
+  validates :answer1, presence: true
+  validates :answer2, presence: true
+  validates :answer3, presence: true
+  validates :answer4, presence: true
+  validates :answer5, presence: true
+  validates :answer6, presence: true
+  validates :answer7, presence: true
+  validates :answer8, presence: true
+  validates :answer9, presence: true
+  validates :answer10, presence: true
+  validates :user, presence: true
+  validates :quiz, presence: true
+  validates :user_id, uniqueness: { scope: :quiz_id }
 end
